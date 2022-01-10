ef3b815f6fd47aeba222b9bc03b94cce634e740e
Hessupatteri <jani.karhula@chasacademy.se>
2022-01-10 11:51:29 +0100

    first try

---
+++ sqlitetable.py
@@ -0,0 +1,66 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def main():
+    database = r"C:\src\sqlite-python-labb\sqldb1.db"
+
+    sql_create_user_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                        user_id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        password text,
+                                        handle text,
+                                        email text
+                                    ); """
+
+    sql_create_repo_table = """CREATE TABLE IF NOT EXISTS tasks (
+                                    repo_id integer PRIMARY KEY,
+                                    url text NOT NULL,
+                                    description text
+                                );"""
+
+    # create a database connection
+    conn = create_connection(database)
+
+    # create tables
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_user_table)
+
+        # create tasks table
+        create_table(conn, sql_create_repo_table)
+    else:
+        print("Error! cannot create the database connection.")
+
+
+if __name__ == '__main__':
+    main()
